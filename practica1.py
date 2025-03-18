c021cb190856086ac0877205af2ad33fbf9d771b
mikfull <mikfull777@gmail.com>
2025-03-18 18:22:03 +0100

    Le he metido los simbolos de los dados pa cada tirada, un cambio de turno y la tirada de dados

--- practica1.py
+++ practica1.py
@@ -9,15 +9,10 @@ class Tablero(object):
         self.crear_tablero_vacio(num_columnas)
         self.añadir_jugadores(fichas, num_fichas)
 
-
     def añadir_jugadores(self,fichas, numero_fichas):
         for i in range(0,len(fichas)):
             self.tablero[i][0] = fichas[i]
             self.tablero[i][1] = numero_fichas
-
-
-
-
 
     def crear_tablero_vacio(self, numero_columnas):
         self.tablero = [["",0] for i in range(numero_columnas)]
@@ -30,6 +25,9 @@ class Pargammon(object):
         self.J = len(fichas) # Numero de jugadores
         self.FICHAS = fichas # Caracteres de las fichas de cada jugador
         self.tablero = Tablero(self.N, self.M, self.FICHAS)
+        self.turno = 0 # Para el wey que le toca tirar
+        self.dados = [] #Para guardar la ultima tirada de dados y esta luego ponerla en un array con todas las tiradas de la partida
+        self.historial_dados = []
 
 
     def __repr__(self) -> str:
@@ -38,11 +36,25 @@ class Pargammon(object):
         """
         # …
 
+    def imagen_dado (self):
+        # devuelve el la cara de dado que haya salido
+        simbolos_dados= ["⚀","⚁","⚂", "⚃", "⚄","⚅"] #https://es.piliapp.com/symbol/dice/
+        resultado = ""
+        for cara_dado in self.dados:
+            resultado += simbolos_dados[cara_dado - 1] + " "
+        return resultado
+
     def cambiar_turno(self) -> bool:
         """ Cambia de turno. Devuelve True si es fin de partida, False si no """
         # …
+        self.turno+= 1 #cambio turno
+        if self.turno >= self.J:
+            self.turno=0
         # La tirada de dados se debe realizar con esta línea:
-        self.dados = [randrange(6) + 1 for _ in range(self.D)]
+        self.dados = [randrange(6) + 1 for _ in range(self.D)] #estas dos lineas de los dados podriamos hacerlo en una funcion pequeñas, como veas
+        self.historial_dados += [self.dados[:]]
+
+        print(f"Turno de {self.FICHAS[self.turno]}: {self.imagen_dado()}")
         # …
 
     def jugar(self, txt_jugada: str) -> None | str:
