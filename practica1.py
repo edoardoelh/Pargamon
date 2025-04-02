00e81e667f57036fceb598bb4c872820883ea1f3
Edgar Lopez Herrera <edoardoelh@gmail.com>
2025-04-02 17:49:59 +0200

    Se ha completado la funcion que genera jugadas posibles, falta hacer que genere la jugada vacia en el caso de que no exista ninguna jugada disponible. Tambien falta terminar de documentar las funciones y hacer que se procese como -1 si la entrada es un @.

--- practica1.py
+++ practica1.py
@@ -1,4 +1,5 @@
 from random import seed, randrange, choice
+import pydoc
 
 AZAR = 75 # Semilla del generador de números aleatorios
 
@@ -15,11 +16,11 @@ class Tablero(object):
         for i in range(altura_tablero):
             salida += ""
             for j in range(len(self.tablero)):
-                if self.tablero[j][0]=="": salida +=" "
+                if self.tablero[j][0] == "": salida += " "
                 else:
                     if self.tablero[j][1] >= altura_tablero - i: salida += self.tablero[j][0]
-                    else: salida +=" "
-                if not j-1==len(self.tablero): salida += "|"
+                    else: salida += " "
+                if not j-1 == len(self.tablero): salida += "|"
             salida += "\n"
         for i in range(len(self.tablero)):
             salida += chr(65+i) + " "
@@ -27,6 +28,12 @@ class Tablero(object):
         return salida
 
     def obtener_altura_tablero(self):
+        """
+        Retorna un numero entero perteneciente a la cantidad de fichas de la columna con mayor numero de los mismas
+
+        :return:
+        altura_tablero(int):Numero correspondiente a la cantidad de fichas de la cosilla con mas fichas
+        """
         altura = 0
         for columna in self.tablero:
             if columna[1] > altura:
@@ -35,91 +42,127 @@ class Tablero(object):
 
 
     def añadir_jugadores(self,fichas, numero_fichas):
+        """
+        Coloca las fichas en el tablero en funcion de los jugadores especificados y el numero de fichas dadas
+        :param fichas: Array de caracteres que seran las representaciones de las fichas de los jugadores
+        :param numero_fichas: Numero de fichas que se le asignaran a cada jugador
+        :return:
+        None
+        """
         for i in range(0,len(fichas)):
             self.tablero[i][0] = fichas[i]
             self.tablero[i][1] = numero_fichas
 
     def crear_tablero_vacio(self, numero_columnas):
+        """
+        Crea un tablero con tantas casillas como se le pasen por parametro
+        :param numero_columnas: Numero de casillas que tendra el tablero
+        :return:
+        """
         self.tablero = [["",0] for i in range(numero_columnas)]
 
-    def obtener_indice_columnas_usables(self, jugador):
+    def obtener_indice_columnas_usables(self, jugador, tablero = None):
+        """
+        Retorna un array de indices de las casillas del tablero que pertenecen al jugador
+        :param jugador: Caracter de la ficha del jugador a consultar
+        :param tablero:
+        :return int[]: Lista de los indices del tablero que pertenecen al jugador consultado
+        """
+        if tablero is None:
+            tablero = self.tablero
         columnas_usables = []
-        for indice in range(len(self.tablero)):
-            if self.tablero[indice][0] == jugador:
+        for indice in range(len(tablero)):
+            if tablero[indice][0] == jugador:
                 columnas_usables.append(indice)
         return columnas_usables
 
     def comprobar_movimiento(self, casilla, movimiento, jugador, tablero = None):
-        if tablero is None:
-            tablero = self.tablero
+        """
+        Retorna un valor booleano correspondiente a si se puede realizar el moviminto solicitado
+        :param casilla: Casilla que contiene la ficha del jugador a mover (De no tratarse de una ficha del jugador la funcion devuelve False)
+        :param movimiento: Entero que determina la distancia a moverse por el jugador
+        :param jugador: Caracter de la ficha del jugador a comprobar
+        :param tablero: (tablero, optional) Tablero en el que realizara la comprobacion, en el caso de que no
+                        se especifique, usara el tablero perteneciente al objeto
+        :return: Boleano que especifica si se puede realizar la jugada consultada o no.
+        """
+
         movimiento_posible = True
-        if casilla + movimiento > len(tablero)+1:
-            movimiento_posible = False #el intento de movimiento se pasa del maximo del tablero
-        if casilla + movimiento < len(tablero):
-            if tablero[casilla][0] != jugador: movimiento_posible = False #La casilla pertenece a otro jugador
-            if tablero[casilla + movimiento][0]!=jugador and tablero[casilla + movimiento][1]>1: movimiento_posible = False #El otro jugador tiene mas de una ficha en su columna
+        if casilla != -1:
+            if tablero is None:
+                tablero = self.tablero
+            if casilla + movimiento > len(tablero)+1:
+                movimiento_posible = False #el intento de movimiento se pasa del maximo del tablero
+            if casilla + movimiento < len(tablero):
+                if tablero[casilla][0] != jugador: movimiento_posible = False #La casilla pertenece a otro jugador
+                if tablero[casilla + movimiento][0] != jugador and tablero[casilla + movimiento][1] > 1: movimiento_posible = False #El otro jugador tiene mas de una ficha en su columna
         return movimiento_posible
 
     def comprobar_movimientos(self, casillas, movimientos, jugador, tablero=None):
-        if tablero is None:
-            #tablero = copy.deepcopy(self.tablero)
-            tablero = self.realizar_copia_tablero(self.tablero)#Esto se encarga de realizar una pseudo sobrecarga del metodo
-        print("Llega aqui")
+        if tablero is None:#Esto se encarga de realizar una pseudo sobrecarga del metodo
+            tablero = self.realizar_copia_tablero()
         movimientos_posibles = True
         tablero_virtual = self.realizar_copia_tablero(tablero)
         for i in range(len(movimientos)):
             if movimientos_posibles:
-                print(self.comprobar_movimiento(casillas[i], movimientos[i], jugador, tablero_virtual))
+                #print(self.comprobar_movimiento(casillas[i], movimientos[i], jugador, tablero_virtual))  #BORRAR POSTERIORMENTE
                 if self.comprobar_movimiento(casillas[i], movimientos[i], jugador, tablero_virtual):
                     self.realizar_movimiento(casillas[i], movimientos[i], jugador, tablero_virtual)
                 else:
                     movimientos_posibles = False
-        print(tablero_virtual)
+        #print(tablero_virtual) #BORRAR POSTERIORMENTE
         return movimientos_posibles
 
 
     def realizar_movimiento(self, casilla, movimiento, jugador, tablero = None):
-        if tablero is None:
-            tablero = self.tablero
         movimiento_realizado = True
-        if not self.comprobar_movimiento(casilla, movimiento, jugador, tablero):
-            movimiento_realizado = False
-        else:
-            if tablero[casilla + movimiento][0]!=jugador and tablero[casilla + movimiento][1]==1:
-                tablero[casilla + movimiento][0] = jugador
-#FALTA: llamada a la funcion que hace que la casilla comida se mueva a donde le corresponda
+        if casilla != -1:
+            if tablero is None:
+                tablero = self.tablero
+            if not self.comprobar_movimiento(casilla, movimiento, jugador, tablero):
+                movimiento_realizado = False
             else:
-                tablero[casilla + movimiento][0] = jugador
-                tablero[casilla + movimiento][1] += 1
-                tablero[casilla][1] -= 1
-                if tablero[casilla][1]==0: tablero[casilla][0] = ''
+                if tablero[casilla + movimiento][0] != jugador and tablero[casilla + movimiento][1] == 1:
+                    tablero[casilla + movimiento][0] = jugador
+#FALTA: llamada a la funcion que hace que la casilla comida se mueva a donde le corresponda
+                else:
+                    tablero[casilla + movimiento][0] = jugador
+                    tablero[casilla + movimiento][1] += 1
+                    tablero[casilla][1] -= 1
+                    if tablero[casilla][1] == 0: tablero[casilla][0] = ''
         return movimiento_realizado
 
     def get_jugadas_posibles(self,dados, jugador):
-        tablero_virtual = self.tablero.copy()
+        #tablero_virtual = self.tablero.copy()
         jugadas_posibles:list = self.generador_jugadas(dados, jugador, self.tablero)
         print("Jugadas posibles totales: [", *jugadas_posibles, "]", sep='\n')
         return jugadas_posibles
 
-    def generador_jugadas(self, dados, jugador, tablero, jugada = None):
+    def generador_jugadas(self, dados, jugador, tablero, jugada = None, jugadas = None):
         if jugada is None: jugada = []
-        jugadas = []
-        for columna in self.obtener_indice_columnas_usables(jugador):#Realiza el movimiento por cada una de las columnas usables
+        if jugadas is None: jugadas = []
+        columnas_usables = self.obtener_indice_columnas_usables(jugador, tablero)
+        for columna in columnas_usables:#Realiza el movimiento por cada una de las columnas usables
             if self.comprobar_movimiento(columna, dados[0], jugador, tablero):#Comprueba si ese movimiento se puede realizar
-                copia_tablero = tablero.copy()
-                copia_dados = dados.copy()
+                copia_tablero = self.realizar_copia_tablero(tablero)
+                copia_dados = dados.copy()#Se realiza una copia de los dados antes de realizar el movimiento, para poder borrarlo de la lista
                 self.realizar_movimiento(columna, copia_dados[0], jugador, copia_tablero)
-                copia_dados.pop()
                 jugada.append(columna)
-                if len(copia_dados)>1:
-                    self.generador_jugadas(copia_dados, jugador, copia_tablero, jugada.append(columna))
+                copia_dados.pop(0)
+                if len(copia_dados) >= 1:
+                    self.generador_jugadas(copia_dados, jugador, copia_tablero, jugada, jugadas)
                 else:
-                    jugadas.append(jugada)
+                    #Aqui se tiene que añadir a la parte final de la jugada el valor de la misma para hacer luego la ordenacion (array.sort(key=lambda L: L[1]), https://www.reddit.com/r/learnpython/comments/loex48/how_to_sort_by_the_second_element_in_a_2d_list/)
+                    jugadas.append(jugada.copy())
+                jugada.pop()
+                copia_tablero = self.realizar_copia_tablero(tablero)
             else:
                 jugada.append(-1)
         return jugadas
 
-    def realizar_copia_tablero(self,tablero):
+    def realizar_copia_tablero(self,tablero = None):
+        if tablero == None:
+            tablero = self.tablero
         return [[j for j in casilla] for casilla in tablero]
             
 
@@ -145,7 +188,7 @@ class Pargammon(object):
 
     def imagen_dado (self):
         # devuelve el la cara de dado que haya salido
-        simbolos_dados= ["⚀","⚁","⚂", "⚃", "⚄","⚅"] #https://es.piliapp.com/symbol/dice/
+        simbolos_dados = ["⚀","⚁","⚂", "⚃", "⚄","⚅"] #https://es.piliapp.com/symbol/dice/
         resultado = ""
         for cara_dado in self.dados:
             resultado += simbolos_dados[cara_dado - 1] + " "
@@ -154,7 +197,7 @@ class Pargammon(object):
     def cambiar_turno(self) -> bool:
         """ Cambia de turno. Devuelve True si es fin de partida, False si no """
         # …
-        self.turno+= 1 #cambio turno
+        self.turno += 1 #cambio turno
         # La tirada de dados se debe realizar con esta línea:
         self.dados = [randrange(6) + 1 for _ in range(self.D)] #estas dos lineas de los dados podriamos hacerlo en una funcion pequeñas, como veas
         self.historial_dados += [self.dados[:]]
@@ -172,7 +215,8 @@ class Pargammon(object):
 #Aqui se tiene que añadir una funcion que compruebe que el texto introducido es valido y que la cantidad de caracteres no supera las de los dados
 
         #Esta funcion esta dando problemas, por ahora me centrare en hacer que funcione la realizacion de jugadas
-        #jugadas_posibles = self.tablero.get_jugadas_posibles(self.dados, jugador_actual)
+        jugadas_posibles = self.tablero.get_jugadas_posibles(self.dados, jugador_actual)
+        print(jugadas_posibles)
 
         while not jugada_realizada:
             movimientos: list = [ord(c.lower()) - ord('a') for c in txt_jugada]
@@ -180,11 +224,12 @@ class Pargammon(object):
                 for i in range(len(movimientos)):
                     jugada_realizada = self.tablero.realizar_movimiento(movimientos[i], self.dados[i], jugador_actual)
             else:
-                txt_jugada = input("Algo ha fallado prueba otra vez: ")
+                txt_jugada = input("Jugada: ")
         # …
 
 def main():
     seed(AZAR)
+    pydoc.writedoc('practica1')
     print("*** PARGAMMON ***")
     params = map(int, input("Numero de columnas, fichas y dados = ").split())
     juego = Pargammon(*params)
